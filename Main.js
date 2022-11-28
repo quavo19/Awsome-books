a2f9d94608a6cfb5687fb16f2cc5352b1233a8d1
AkiteDonald <donaldakite27@gmail.com>
2022-11-28 14:53:35 +0000

    Add script for addbook

--- Main.js
+++ Main.js
@@ -1,8 +1,36 @@
-const Button = document.getElementById('Add-Button');
-const Title = document.getElementById('Book-title');
-const Author = document.getElementById('Book-author');
+const book = [];
+function CreateBook(titleValue, authorValue) {
+  this.title = titleValue;
+  this.author = authorValue;
+};
+const pushBook = () => {
+  const titleValue = document.getElementById('Title').value;
+  const authorValue = document.getElementById('Author').value;
+  const theBook = new CreateBook(titleValue, authorValue);
+  book.push(theBook);
+  console.log(book);
+};
+const bookList = document.querySelector('.books-list');
+const createBook = () => {
+  for (let i = 0; i < book.length; i += 1) {
+    const title = document.createElement('p');
+    title.innerText = book[i].title;
+    bookList.append(title);
 
-Button.addEventListener('click', () => {
-  Title.innerHTML = 'Book';
-  Author.innerHTML = 'Auther';
+    const author = document.createElement('p');
+    author.innerText = book[i].author;
+    bookList.append(author);
+
+    const removeBtn = document.createElement('button');
+    removeBtn.innerText = 'Remove';
+    bookList.append(removeBtn);
+
+    const line = document.createElement('hr');
+    bookList.append(line);
+  }
+};
+const addBtn = document.querySelector('#Add-Button');
+addBtn.addEventListener('click', () => {
+  pushBook();
+  createBook();
 });
