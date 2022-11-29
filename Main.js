1b75c576a3719882d11f91b56c44db000255fa01
AkiteDonald <donaldakite27@gmail.com>
2022-11-29 10:59:37 +0000

    fix linter errors

--- Main.js
+++ Main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-loop-func */
 let book = [];
 
 function CreateBook(titleValue, authorValue, id) {
@@ -7,30 +8,30 @@ function CreateBook(titleValue, authorValue, id) {
 }
 
 const pushBook = () => {
-  const titleValue = document.getElementById("Title").value;
-  const authorValue = document.getElementById("Author").value;
+  const titleValue = document.getElementById('Title').value;
+  const authorValue = document.getElementById('Author').value;
   const id = book.length;
   const theBook = new CreateBook(titleValue, authorValue, id);
   book.push(theBook);
 };
 
-const bookList = document.querySelector(".books-list");
+const bookList = document.querySelector('.books-list');
 const createBook = () => {
-  bookList.innerHTML = " ";
+  bookList.innerHTML = ' ';
   for (let i = 0; i < book.length; i += 1) {
-    const title = document.createElement("p");
+    const title = document.createElement('p');
     title.innerText = book[i].title;
     bookList.append(title);
 
-    const author = document.createElement("p");
+    const author = document.createElement('p');
     author.innerText = book[i].author;
     bookList.append(author);
 
-    const removeBtn = document.createElement("button");
-    removeBtn.innerText = "Remove";
+    const removeBtn = document.createElement('button');
+    removeBtn.innerText = 'Remove';
     bookList.append(removeBtn);
     removeBtn.id = book[i].id;
-    const removeBook = (e) => {
+    const removeBook = () => {
       const target = removeBtn.id;
       book.splice(target, 1);
 
@@ -43,26 +44,27 @@ const createBook = () => {
       }
     };
 
-    removeBtn.addEventListener("click", () => {
-      book = JSON.parse(localStorage.getItem("book")) || [];
+    removeBtn.addEventListener('click', () => {
+      book = JSON.parse(localStorage.getItem('book')) || [];
       removeBook();
+      // eslint-disable-next-line no-use-before-define
       store();
       window.location.reload();
     });
 
-    const line = document.createElement("hr");
+    const line = document.createElement('hr');
     bookList.append(line);
   }
 };
 const store = () => {
-  localStorage.setItem("book", JSON.stringify(book));
+  localStorage.setItem('book', JSON.stringify(book));
 };
-const addBtn = document.querySelector("#Add-Button");
-addBtn.addEventListener("click", () => {
+const addBtn = document.querySelector('#Add-Button');
+addBtn.addEventListener('click', () => {
   pushBook();
   store();
   window.location.reload();
 });
 
-book = JSON.parse(localStorage.getItem("book")) || [];
+book = JSON.parse(localStorage.getItem('book')) || [];
 createBook();
