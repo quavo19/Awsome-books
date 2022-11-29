d6ebd42bbee07c75cf54b67abce2be1cc0227a84
Outkarm <108669691+Outkarm@users.noreply.github.com>
2022-11-29 10:38:45 +0000

    add function for remove button

--- Main.js
+++ Main.js
@@ -1,36 +1,68 @@
-const book = [];
-function CreateBook(titleValue, authorValue) {
+let book = [];
+
+function CreateBook(titleValue, authorValue, id) {
   this.title = titleValue;
   this.author = authorValue;
+  this.id = id;
 }
+
 const pushBook = () => {
-  const titleValue = document.getElementById('Title').value;
-  const authorValue = document.getElementById('Author').value;
-  const theBook = new CreateBook(titleValue, authorValue);
+  const titleValue = document.getElementById("Title").value;
+  const authorValue = document.getElementById("Author").value;
+  const id = book.length;
+  const theBook = new CreateBook(titleValue, authorValue, id);
   book.push(theBook);
 };
-const bookList = document.querySelector('.books-list');
+
+const bookList = document.querySelector(".books-list");
 const createBook = () => {
-  bookList.innerHTML = ' ';
+  bookList.innerHTML = " ";
   for (let i = 0; i < book.length; i += 1) {
-    const title = document.createElement('p');
+    const title = document.createElement("p");
     title.innerText = book[i].title;
     bookList.append(title);
 
-    const author = document.createElement('p');
+    const author = document.createElement("p");
     author.innerText = book[i].author;
     bookList.append(author);
 
-    const removeBtn = document.createElement('button');
-    removeBtn.innerText = 'Remove';
+    const removeBtn = document.createElement("button");
+    removeBtn.innerText = "Remove";
     bookList.append(removeBtn);
+    removeBtn.id = book[i].id;
+    const removeBook = (e) => {
+      const target = removeBtn.id;
+      book.splice(target, 1);
 
-    const line = document.createElement('hr');
+      let theId = 0;
+      if (book.length > 0) {
+        book.forEach((m) => {
+          m.id = theId;
+          theId += 1;
+        });
+      }
+    };
+
+    removeBtn.addEventListener("click", () => {
+      book = JSON.parse(localStorage.getItem("book")) || [];
+      removeBook();
+      store();
+      window.location.reload();
+    });
+
+    const line = document.createElement("hr");
     bookList.append(line);
   }
 };
-const addBtn = document.querySelector('#Add-Button');
-addBtn.addEventListener('click', () => {
+const store = () => {
+  localStorage.setItem("book", JSON.stringify(book));
+};
+const addBtn = document.querySelector("#Add-Button");
+addBtn.addEventListener("click", () => {
   pushBook();
-  createBook();
+  store();
+  window.location.reload();
 });
+
+book = JSON.parse(localStorage.getItem("book")) || [];
+createBook();
